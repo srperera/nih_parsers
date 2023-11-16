c241481bcf3b13dbc360977cbcf7550d13784761
srperera <perera.r.shehan@gmail.com>
2023-11-15 20:19:44 -0500

    update

---
+++ latest_parser/tests/test_surface_parser.py
@@ -0,0 +1,8 @@
+import unittest
+import pandas as pd
+from parsers.surface_parser import SurfaceParserDistributed
+
+
+###########################################################################
+class TestSurfaceParser(unittest.TestCase):
+    pass
